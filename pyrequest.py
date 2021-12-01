9f27881c116e5ea6cf9f69795beffbf92274f88b
Zack D <zack.dean@gmail.com>
2021-11-30 21:04:39 -0500

    first commit

---
+++ pyrequest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import requests
+import json
+
+response = requests.get('http://trivia-api.thedundies.com/questions/')
+
+json_data = json.loads(response.text)
+
+#print (json.dumps(j, indent = 4, sort_keys = True))
+
+print (type(json_data))
+
+print (response.json().get("question"))
