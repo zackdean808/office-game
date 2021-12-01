e9b88c3973f636311e18e703365351ae68193ad1
Zack D <zack.dean@gmail.com>
2021-11-30 22:11:06 -0500

    JSON requests are working will add more later

--- pyrequest.py
+++ pyrequest.py
@@ -5,14 +5,22 @@ import json
 
 response = requests.get('http://trivia-api.thedundies.com/questions/')
 
-json_data = json.loads(response.text)
+# TM doesn't send back a json object that's recognized by python. 
+# Strip off the leading [ and trailing ] 
+# Now python sees this as a dict 
+# Proceed to get individual sections. 
+sanatized_response = response.text.lstrip("[").rstrip("]")
 
-#print (json.dumps(j, indent = 4, sort_keys = True))
+json_data = json.loads(sanatized_response)
 
-print (type(json_data))
+# print the question id
+print (json_data["question_id"])
 
-print (response.json().get("question"))
+# print the question 
+print (json_data["question"])
 
-#new comment
-
-# new new comment 
+# print the answers and correct value 
+print (json_data["answers"][0]["answer"], json_data["answers"][0]["correct"])
+print (json_data["answers"][1]["answer"], json_data["answers"][1]["correct"])
+print (json_data["answers"][2]["answer"], json_data["answers"][2]["correct"])
+print (json_data["answers"][3]["answer"], json_data["answers"][3]["correct"])
