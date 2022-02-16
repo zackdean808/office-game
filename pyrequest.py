4fdb6925dc01abe8288f62fb376fe4a9dec136d4
Zack D <zack.dean@gmail.com>
2022-02-16 12:33:43 -0500

    Partial refactor

--- pyrequest.py
+++ pyrequest.py
@@ -4,72 +4,82 @@ import requests
 import json
 import random
 
-response = requests.get('http://trivia-api.thedundies.com/questions/')
+DEBUG = False
 
-# TM doesn't send back a json object that's recognized as a dict by python.
-# It shows as a list 
-# Strip off the leading [ and trailing ] 
-# Now python sees this as a dict 
-# Proceed to get individual sections. 
-sanatized_response = response.text.lstrip("[").rstrip("]")
+def get_api_response():
+    response = requests.get('http://trivia-api.thedundies.com/questions/')
+    # TM doesn't send back a json object that's recognized as a dict by python.
+    # It shows as a list 
+    # Strip off the leading [ and trailing ] 
+    # Now python sees this as a dict 
+    # Proceed to get individual sections. 
+    sanatized_response = response.text.lstrip("[").rstrip("]")
+    
+    json_data = json.loads(sanatized_response)
+    return json_data
 
-json_data = json.loads(sanatized_response)
+def print_debug_info(json_data):
+        print (json_data["question_id"])
+        # print the answers and correct value 
+        print (json_data["answers"][0]["answer"], json_data["answers"][0]["correct"])
+        print (json_data["answers"][1]["answer"], json_data["answers"][1]["correct"])
+        print (json_data["answers"][2]["answer"], json_data["answers"][2]["correct"])
+        print (json_data["answers"][3]["answer"], json_data["answers"][3]["correct"])
+        print ("")
+        print ("")
 
-# print the question id
-print (json_data["question_id"])
 
-# print the question 
-print (json_data["question"])
+if __name__ == "__main__":
+    json_data = get_api_response()
 
-# print the answers and correct value 
-#print (json_data["answers"][0]["answer"], json_data["answers"][0]["correct"])
-#print (json_data["answers"][1]["answer"], json_data["answers"][1]["correct"])
-#print (json_data["answers"][2]["answer"], json_data["answers"][2]["correct"])
-#print (json_data["answers"][3]["answer"], json_data["answers"][3]["correct"])
+    # print the question id
+    if DEBUG == True:
+        print_debug_info(json_data)
 
-#print ("")
-#print ("")
+    # print the question 
+    print (json_data["question"])
 
-ql = []
+    ql = []
 
-for i in (1,2,3,4):
-    t = i - 1
-    ql.append([json_data["answers"][t]["answer"], json_data["answers"][t]["correct"]])
+    for i in (1,2,3,4):
+        t = i - 1
+        ql.append([json_data["answers"][t]["answer"], json_data["answers"][t]["correct"]])
 
-random.shuffle(ql)
-#print (ql)
+    random.shuffle(ql)
+    #print (ql)
 
-for i in (0,1,2,3):
-    uc = i + 1
-    print(uc, ql[i][0])
+    for i in (0,1,2,3):
+        uc = i + 1
+        print(uc, ql[i][0])
 
-while True:
-    try:
-        user_choice = int(input("Select an answer: "))                     
-        break
-    except ValueError:
-        print("Please input integer only...")  
-        continue
+    while True:
+        try:
+            user_choice = int(input("Select an answer: ")) 
+            break
+        except ValueError:
+            print("Please input integer only...")  
+            continue
 
-if (user_choice == 1):
-    if (ql[0][1] == True):
-        print("Correct")
+    if (user_choice == 1):
+        if (ql[0][1] == True):
+            print("Correct")
+        else:
+            print ("False")
+    elif (user_choice == 2):
+        if (ql[1][1] == True):
+            print("Correct")
+        else:
+            print ("False")
+    elif (user_choice == 3):
+        if (ql[2][1] == True):
+            print("Correct")
+        else:
+            print ("False")
+    elif (user_choice == 4):
+        if (ql[3][1] == True):
+            print("Correct")
+        else:
+            print ("False")
     else:
-        print ("False")
-elif (user_choice == 2):
-    if (ql[1][1] == True):
-        print("Correct")
-    else:
-        print ("False")
-elif (user_choice == 3):
-    if (ql[2][1] == True):
-        print("Correct")
-    else:
-        print ("False")
-elif (user_choice == 4):
-    if (ql[3][1] == True):
-        print("Correct")
-    else:
-        print ("False")
-else:
-    print ("outer false")
+        print ("outer false")
+
